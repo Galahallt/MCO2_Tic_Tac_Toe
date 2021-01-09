4c2828f174c974540e10eafd5df492eaf8652407
NRu0512 <jamesandrew.perez@gmail.com>
2021-01-09 20:45:19 +0800

    XOXO

--- src/Controller.java
+++ src/Controller.java
@@ -25,9 +25,10 @@ public class Controller {
     private Stage window = Main.getWindow();
 
     private int level;
-
+    private int turnCtr;
 
     public void playClicked() throws IOException {
+        turnCtr = 0;
         level = Integer.parseInt(lvlNum.getText());
         System.out.println("Level: " + level);
 
@@ -39,66 +40,156 @@ public class Controller {
     }
 
     public void click1() {
-        ImageView X = new ImageView("X.png");
-        X.setFitHeight(105);
-        X.setFitWidth(110);
-        stck1.getChildren().add(X);
+        if (turnCtr % 2 == 0) {
+            ImageView X = new ImageView("X.png");
+            X.setFitHeight(105);
+            X.setFitWidth(110);
+            stck1.getChildren().add(X);
+        }
+        else {
+            ImageView O = new ImageView("O.png");
+            O.setFitHeight(105);
+            O.setFitWidth(110);
+            stck1.getChildren().add(O);
+        }
+
+        turnCtr++;
         System.out.println("One");
     }
     public void click2() {
-        ImageView X = new ImageView("X.png");
-        X.setFitHeight(105);
-        X.setFitWidth(110);
-        stck2.getChildren().add(X);
+        if (turnCtr % 2 == 0) {
+            ImageView X = new ImageView("X.png");
+            X.setFitHeight(105);
+            X.setFitWidth(110);
+            stck2.getChildren().add(X);
+        }
+        else {
+            ImageView O = new ImageView("O.png");
+            O.setFitHeight(105);
+            O.setFitWidth(110);
+            stck2.getChildren().add(O);
+        }
+
+        turnCtr++;
         System.out.println("Two");
     }
     public void click3() {
-        ImageView X = new ImageView("X.png");
-        X.setFitHeight(105);
-        X.setFitWidth(110);
-        stck3.getChildren().add(X);
+        if (turnCtr % 2 == 0) {
+            ImageView X = new ImageView("X.png");
+            X.setFitHeight(105);
+            X.setFitWidth(110);
+            stck3.getChildren().add(X);
+        }
+        else {
+            ImageView O = new ImageView("O.png");
+            O.setFitHeight(105);
+            O.setFitWidth(110);
+            stck3.getChildren().add(O);
+        }
+
+        turnCtr++;
         System.out.println("Three");
     }
     public void click4() {
-        ImageView X = new ImageView("X.png");
-        X.setFitHeight(105);
-        X.setFitWidth(110);
-        stck4.getChildren().add(X);
+        if (turnCtr % 2 == 0) {
+            ImageView X = new ImageView("X.png");
+            X.setFitHeight(105);
+            X.setFitWidth(110);
+            stck4.getChildren().add(X);
+        }
+        else {
+            ImageView O = new ImageView("O.png");
+            O.setFitHeight(105);
+            O.setFitWidth(110);
+            stck4.getChildren().add(O);
+        }
+
+        turnCtr++;
         System.out.println("Four");
     }
     public void click5() {
-        ImageView X = new ImageView("X.png");
-        X.setFitHeight(105);
-        X.setFitWidth(110);
-        stck5.getChildren().add(X);
+        if (turnCtr % 2 == 0) {
+            ImageView X = new ImageView("X.png");
+            X.setFitHeight(105);
+            X.setFitWidth(110);
+            stck5.getChildren().add(X);
+        }
+        else {
+            ImageView O = new ImageView("O.png");
+            O.setFitHeight(105);
+            O.setFitWidth(110);
+            stck5.getChildren().add(O);
+        }
+
+        turnCtr++;
         System.out.println("Five");
     }
     public void click6() {
-        ImageView X = new ImageView("X.png");
-        X.setFitHeight(105);
-        X.setFitWidth(110);
-        stck6.getChildren().add(X);
+        if (turnCtr % 2 == 0) {
+            ImageView X = new ImageView("X.png");
+            X.setFitHeight(105);
+            X.setFitWidth(110);
+            stck6.getChildren().add(X);
+        }
+        else {
+            ImageView O = new ImageView("O.png");
+            O.setFitHeight(105);
+            O.setFitWidth(110);
+            stck6.getChildren().add(O);
+        }
+
+        turnCtr++;
         System.out.println("Six");
     }
     public void click7() {
-        ImageView X = new ImageView("X.png");
-        X.setFitHeight(105);
-        X.setFitWidth(110);
-        stck7.getChildren().add(X);
+        if (turnCtr % 2 == 0) {
+            ImageView X = new ImageView("X.png");
+            X.setFitHeight(105);
+            X.setFitWidth(110);
+            stck7.getChildren().add(X);
+        }
+        else {
+            ImageView O = new ImageView("O.png");
+            O.setFitHeight(105);
+            O.setFitWidth(110);
+            stck7.getChildren().add(O);
+        }
+
+        turnCtr++;
         System.out.println("Seven");
     }
     public void click8() {
-        ImageView X = new ImageView("X.png");
-        X.setFitHeight(105);
-        X.setFitWidth(110);
-        stck8.getChildren().add(X);
+        if (turnCtr % 2 == 0) {
+            ImageView X = new ImageView("X.png");
+            X.setFitHeight(105);
+            X.setFitWidth(110);
+            stck8.getChildren().add(X);
+        }
+        else {
+            ImageView O = new ImageView("O.png");
+            O.setFitHeight(105);
+            O.setFitWidth(110);
+            stck8.getChildren().add(O);
+        }
+
+        turnCtr++;
         System.out.println("Eight");
     }
     public void click9() {
-        ImageView X = new ImageView("X.png");
-        X.setFitHeight(105);
-        X.setFitWidth(110);
-        stck9.getChildren().add(X);
+        if (turnCtr % 2 == 0) {
+            ImageView X = new ImageView("X.png");
+            X.setFitHeight(105);
+            X.setFitWidth(110);
+            stck9.getChildren().add(X);
+        }
+        else {
+            ImageView O = new ImageView("O.png");
+            O.setFitHeight(105);
+            O.setFitWidth(110);
+            stck9.getChildren().add(O);
+        }
+
+        turnCtr++;
         System.out.println("Nine");
     }
 }
