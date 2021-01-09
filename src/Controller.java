665cb8788d8b417ab6a92855932ff69460c13c37
Galahallt <paolo_edni_v_espiritu@dlsu.edu.ph>
2021-01-09 20:37:18 +0800

    Update tic tac toe

--- src/Controller.java
+++ src/Controller.java
@@ -4,26 +4,28 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class Controller {
     @FXML private TextField lvlNum;
 
-    @FXML private Button btn1;
-    @FXML private Button btn2;
-    @FXML private Button btn3;
-    @FXML private Button btn4;
-    @FXML private Button btn5;
-    @FXML private Button btn6;
-    @FXML private Button btn7;
-    @FXML private Button btn8;
-    @FXML private Button btn9;
+    @FXML private StackPane stck1;
+    @FXML private StackPane stck2;
+    @FXML private StackPane stck3;
+    @FXML private StackPane stck4;
+    @FXML private StackPane stck5;
+    @FXML private StackPane stck6;
+    @FXML private StackPane stck7;
+    @FXML private StackPane stck8;
+    @FXML private StackPane stck9;
 
     private Stage window = Main.getWindow();
 
     private int level;
+
 
     public void playClicked() throws IOException {
         level = Integer.parseInt(lvlNum.getText());
@@ -37,30 +39,66 @@ public class Controller {
     }
 
     public void click1() {
+        ImageView X = new ImageView("X.png");
+        X.setFitHeight(105);
+        X.setFitWidth(110);
+        stck1.getChildren().add(X);
         System.out.println("One");
     }
     public void click2() {
+        ImageView X = new ImageView("X.png");
+        X.setFitHeight(105);
+        X.setFitWidth(110);
+        stck2.getChildren().add(X);
         System.out.println("Two");
     }
     public void click3() {
+        ImageView X = new ImageView("X.png");
+        X.setFitHeight(105);
+        X.setFitWidth(110);
+        stck3.getChildren().add(X);
         System.out.println("Three");
     }
     public void click4() {
+        ImageView X = new ImageView("X.png");
+        X.setFitHeight(105);
+        X.setFitWidth(110);
+        stck4.getChildren().add(X);
         System.out.println("Four");
     }
     public void click5() {
+        ImageView X = new ImageView("X.png");
+        X.setFitHeight(105);
+        X.setFitWidth(110);
+        stck5.getChildren().add(X);
         System.out.println("Five");
     }
     public void click6() {
+        ImageView X = new ImageView("X.png");
+        X.setFitHeight(105);
+        X.setFitWidth(110);
+        stck6.getChildren().add(X);
         System.out.println("Six");
     }
     public void click7() {
+        ImageView X = new ImageView("X.png");
+        X.setFitHeight(105);
+        X.setFitWidth(110);
+        stck7.getChildren().add(X);
         System.out.println("Seven");
     }
     public void click8() {
+        ImageView X = new ImageView("X.png");
+        X.setFitHeight(105);
+        X.setFitWidth(110);
+        stck8.getChildren().add(X);
         System.out.println("Eight");
     }
     public void click9() {
+        ImageView X = new ImageView("X.png");
+        X.setFitHeight(105);
+        X.setFitWidth(110);
+        stck9.getChildren().add(X);
         System.out.println("Nine");
     }
 }
